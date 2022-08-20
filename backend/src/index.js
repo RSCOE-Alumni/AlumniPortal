76a886a5f13c08ae109031389af42e11b59254a4
Alumni <alumni@jspmrscoe.edu.in>
2022-08-20 17:16:38 +0530

    configure github actions 1

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,15 +6,21 @@ const feedRouter = require("./routers/feed");
 const admin = require("./routers/admin");
 const app = express();
 
+const path = require("path");
+
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../../frontend/build")));
+
+// console.log(__dirname);
 
 app.use(userRouter);
 app.use(admin);
 app.use(feedRouter);
 
 app.listen(port, () => {
-	console.log(`Server is up on port ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
